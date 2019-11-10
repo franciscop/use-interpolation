ecf88919cb059eb938bd160908889e16249d8ec0
Francisco Presencia <public@francisco.io>
2019-11-10 20:51:44 +0900

    Upgraded and added dep

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Based off a tweet and codesandbox:
 // https://mobile.twitter.com/hieuhlc/status/1164369876825169920
 import { useRef } from "react";
+import Ola from "ola";
 
 // Reusable component that also takes dependencies
 export default time => {
