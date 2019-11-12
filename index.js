fd264152b5974741ad02b73f1d9334521ee97102
Francisco Presencia <public@francisco.io>
2019-11-12 22:55:03 +0900

    Added better interpolation

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 // Based off a tweet and codesandbox:
 // https://mobile.twitter.com/hieuhlc/status/1164369876825169920
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Ola from "ola";
+import useAnimationFrame from "use-animation-frame";
 
 // Reusable component that also takes dependencies
-export default time => {
-  const ref = useRef();
-  const setValue = value => {
-    if (ref.current) {
-      ref.current.value = value;
+export default (init, time) => {
+  const ref = useRef(new Ola(init, time));
+
+  const [val, setVal] = useState(init);
+  const [hard, setHard] = useState(init);
+
+  const setUserValue = value => {
+    if (typeof value === "function") {
+      ref.current.value = value(hard);
     } else {
-      ref.current = new Ola(value, time);
+      ref.current.value = value;
     }
+    setHard(value);
   };
-  return [ref.current, setValue];
+
+  useAnimationFrame(() => {
+    const value = ref.current.value;
+    if (value !== val) {
+      setVal(value);
+    }
+  }, []);
+
+  return [val, setUserValue, hard];
 };
